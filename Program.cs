b36d923362f9cd6699ebf81825b39f31f7e545d2
Elijah Lekomo <lekomoayomide@gmail.com>
2025-07-03 00:22:07 +0200

    Made changes to folder structure

--- Program.cs
+++ Program.cs
@@ -2,52 +2,61 @@ using System.Globalization;
 using Microsoft.EntityFrameworkCore;
 using Poultry_management_System.Data;
 
-var cultureInfo = new CultureInfo("en-ZA");
-cultureInfo.NumberFormat.CurrencyDecimalSeparator = ".";
-cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
+namespace Poultry_management_System {
 
-CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cultureInfo = new CultureInfo("en-ZA");
+            cultureInfo.NumberFormat.CurrencyDecimalSeparator = ".";
+            cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 
-var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
-{
-    hostingContext.HostingEnvironment.EnvironmentName = Environments.Production;
-});
+            builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                hostingContext.HostingEnvironment.EnvironmentName = Environments.Production;
+            });
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<DataContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("LeburuPoultryFarm")));
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("LeburuPoultryFarm")));
 
-var app = builder.Build();
+            var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            /*app.MapControllerRoute(
+                name: "area",
+                pattern: "{area:exists}/{controller}/{action}/{id?}");*/
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-/*app.MapControllerRoute(
-    name: "area",
-    pattern: "{area:exists}/{controller}/{action}/{id?}");*/
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
--- obj/Debug/net9.0/Poultry management System.AssemblyInfo.cs
+++ obj/Debug/net9.0/Poultry management System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Poultry management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c9fe9800345faa1d84c75689a48114d35b1730")]
 [assembly: System.Reflection.AssemblyProductAttribute("Poultry management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Poultry management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
