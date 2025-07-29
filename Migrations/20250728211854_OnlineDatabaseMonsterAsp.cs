3b542fe51695357a14500acb043a9f882bd10cfa
Elijah Lekomo <lekomoayomide@gmail.com>
2025-07-29 21:01:37 +0200

    -Added new images -changed database connection string to online database(MonsterAsp.net) -Added more responsive design for mobile to landing page, hero and about us section

---
+++ Migrations/20250728211854_OnlineDatabaseMonsterAsp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Poultry_management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class OnlineDatabaseMonsterAsp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
